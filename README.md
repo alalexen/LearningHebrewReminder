47ec3b50dbb9a35a366eb92f19adf168421ea991
Alex45682 <alexandraalexeenko647@gmail.com>
2023-10-30 00:08:32 +0200

    fix addBlock handeling

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 To add:
 1. scoring
 2. GUI with tkinter
-3. timer for reminder
+~~3. timer for reminder~~
 4. binyanim for each word
 5. some rules as a tooltips
-6. writing words to file from pealim app
+~~6. writing words to file from pealim app~~
 7. sentenses to translate
 8. files with words divided by topics
 
--------
+-----------------------------------------
 can be used with a crontab
+
+-----------------------------------------
+How to add AddBlock:
+
+1. To download and save the extension as .crx file
+2. Add option to the driver: driver.add_extension('User/path_to_file.crx')
+
--- add_words_from_pealim.py
+++ add_words_from_pealim.py
@@ -9,8 +9,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.common.by import By as by
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
 
 url = "https://www.pealim.com/search/?q=%s"
 search_word_input = "//input[@id='search-box']"
@@ -19,32 +17,32 @@ view_full_conjugation = "//a[@class='btn btn-primary' and contains(text(), 'View
 active_forms_title = "//h3[@class='page-header' and contains(text(), 'Active forms')]"
 
 class BasePage:
-    def __init__(self, timeout):
+    def __init__(self, timeout=15):
 
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument("--incognito")
         chrome_options.add_extension('/Users/store/Desktop/extensions/1.9.6_0.crx')
 
         self.driver = webdriver.Chrome(options=chrome_options)
-
         self.wait = WebDriverWait(self.driver, timeout=timeout)
-
 
     def open_pealim(self, word) -> None:
         """ Open browser and navigate to pealim app"""
         self.driver.get(url % word)
         self.driver.get(url % word)
         self.driver.maximize_window()
-        sleep(10)
+        sleep(7)
 
-    def close_addBlock_tabs(self):
-        all_handles = self.driver.window_handles
-        for handle in all_handles:
-            self.driver.switch_to.window(handle)
+    def close_addBlock_tabs(self) -> None:
+        """ Close all tabs that were opened with addBlock extension, stitch to the main tab"""
+        tabs = self.driver.window_handles
+        for t in tabs:
+            self.driver.switch_to.window(t)
             if "Blocker" in self.driver.title:
                 self.driver.close()
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        self.driver.refresh()
 
-    def click_on_element(self, xpath):
+    def click_on_element(self, xpath) -> None:
         """ Wait until element is displayed and click """
         element = self.wait_until_element_displayed(xpath)
         element.click()
@@ -54,7 +52,7 @@ class BasePage:
             EC.presence_of_element_located((by.XPATH, element)))
         return element
 
-    def fill_input_with_text(self, input_xpath, text):
+    def fill_input_with_text(self, input_xpath, text) -> None:
         """ Wait until input is displayed and fill the text """
         element = self.wait_until_element_displayed(input_xpath)
         element.send_keys(text)
@@ -62,7 +60,7 @@ class BasePage:
 
 class AddWord(BasePage):
 
-    def test_view_full_conjugation(self):
+    def test_view_full_conjugation(self) -> None:
         self.click_on_element(view_full_conjugation)
         self.wait_until_element_displayed(active_forms_title)
 
